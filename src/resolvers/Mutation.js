82efe9d637cadc82ae506816e5dc7bf6d8f1a7e9
KrishnaChittodia21 <krishnachittodia.10@gmail.com>
2021-04-26 00:27:38 +0530

    added generateToken function

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import getUserId from '../utils/getUserId';
+import generateToken from '../utils/generateToken';
 
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
@@ -18,7 +18,7 @@ const Mutation = {
 
     return {
       user,
-      token: jwt.sign({ userId: user.id }, 'mysecret')
+      token: generateToken(user.id)
     }
   },
   async login(parent, args, { prisma }, info) {
@@ -36,7 +36,7 @@ const Mutation = {
     }
     return {
       user,
-      token: jwt.sign({ userId: user.id}, 'mysecret')
+      token: generateToken(user.id)
     }
   },
   async createPost(parent, args, { prisma, request }, info) {
