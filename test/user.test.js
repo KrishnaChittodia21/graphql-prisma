2911f8332d4c9aed20953a353f811ea9631aa623
KrishnaChittodia21 <krishnachittodia.10@gmail.com>
2021-04-28 15:44:55 +0530

    added test cases

--- test/user.test.js
+++ test/user.test.js
@@ -1,5 +1,32 @@
-import 'cross-fetch/polyfill';
-import ApolloBoost, { gql } from 'apollo-boost';
+import "cross-fetch/polyfill";
+import ApolloBoost, { gql } from "apollo-boost";
+import prisma from '../src/prisma';
 
 const client = new ApolloBoost({
-})
+  uri: "http://localhost:4000"
+});
+
+test('should create a new user', async () => {
+  const createUser = gql`
+    mutation {
+      createUser(
+        data: {
+          name: "dollu2120",
+          email: "dollu11@dollu11.com",
+          password: "k123456789",
+        }
+      ){
+        token,
+        user {
+          id
+        }
+      }
+    }
+  `;
+
+  const response = await client.mutate({
+    mutation: createUser
+  })
+  const exists = await prisma.exists.User({ id: response.data.createUser.user.id });
+  expect(exists).toBe(true);
+}, 10000)
